ddb212fe78d177ab179ed9484933dd8e3f5ea041
cleanhead01 <seokjh93@naver.com>
2020-09-18 17:35:22 +0900

    수업내용 업로드

---
+++ js/common.js
@@ -0,0 +1,30 @@
+(function($){
+
+    $(window).load(function(){
+        $('.introAni').delay(500).fadeOut(500)
+    })
+
+    $('#containerBox').load('main.html')
+    
+    $('.depth1 > li').hover(
+        function(){
+            $(this).find('.depth2')
+            .stop().slideDown(700)
+        },
+        function(){
+            $(this).find('.depth2')
+            .stop().slideUp(500)
+        }
+    )
+
+    $('.login a, h1 a').on('click', function(e){
+        e.preventDefault();
+        var url = $(this).attr('href');
+        $('#container').remove()
+        $('#containerBox').load(url)
+    })
+
+
+
+
+})(jQuery)
