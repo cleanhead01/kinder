3f9a8f8a6190e54810cd355abde885a901d80a03
cleanhead01 <seokjh93@naver.com>
2020-09-18 17:36:59 +0900

    수업내용 업로드

--- js/login.js
+++
@@ -1,9 +0,0 @@
-$('.login_form').on('submit', function(e){
-  e.preventDefalut()
-  var details = $('.login_form').serialize();
-  $.post('register.php', details, function(){
-    $('.topmenu .login').removeClass('on')
-    $('.topmenu .logout').addClass('on')
-  })
-
-})
